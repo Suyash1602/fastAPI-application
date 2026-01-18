9f363c1e4445093c40453a2a530a14ae225d2db4
Suyash <yashkhandalkar04@gmail.com>
2026-01-18 17:38:00 +0530

    Setting up alembic and models

--- backend/core/config.py
+++ backend/core/config.py
@@ -9,13 +9,13 @@ class Settings:
     PROJECT_TITLE: str = "FastAPI Application"
     PROJECT_VERSION: str = "0.1.0" 
 
-    # DATABASE_URL: str = "sqlite:///./sql_app.db"
+    DATABASE_URL: str = "sqlite:///./sql_app.db"
 
     POSTGRES_USER: str = os.getenv("POSTGRES_USER")
     POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD")
     POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER","localhost")
     POSTGRES_PORT: int = os.getenv("POSTGRES_PORT",5432)
     POSTGRES_DB: str = os.getenv("POSTGRES_DB")
-    DATABASE_URL: str = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+    # DATABASE_URL: str = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
 settings = Settings()
